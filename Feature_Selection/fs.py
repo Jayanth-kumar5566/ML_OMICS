762ac24603bd3c933667e2f62915b5dfdeecc753
Jayanth-kumar5566 <jayanth.kumar@students.iiserpune.ac.in>
2018-06-07 06:30:56 +0530

    MFORD-3 added the feature selection codes and ran it for fungal and microbial datasets. Also copy pasted the status of the individual in the output files. Project webpage is also updated

--- Feature_Selection/fs.py
+++ Feature_Selection/fs.py
@@ -1,7 +1,21 @@
 import pandas
-df=pandas.read_csv("../Demographics.1.csv")
+df=pandas.read_csv("../bacterial_microbiome.csv")
 
 #====Finding the Maximum of the columns=========
 
-print df.max()
+df_max=df.max().to_dict()
+cutoff=1 #Set the cutoff here
+cols=[] #contains all the filtered columns
+
+for i in df_max:
+    if df_max[str(i)]>=1:
+        cols.append(str(i))
+    else:
+        pass
+
+df_filtered=df[cols]
+
+#==========Save it as a csv file===========
+df_filtered=df_filtered.set_index("PatientID")
+df_filtered.to_csv("./Selected/bacterial_microbiome.csv")
 
