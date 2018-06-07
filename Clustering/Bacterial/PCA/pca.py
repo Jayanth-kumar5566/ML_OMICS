8c6cf79a20df68bb927475d3b237502a63c4a9e8
Jayanth-kumar5566 <jayanth.kumar@students.iiserpune.ac.in>
2018-06-07 08:24:58 +0530

    Tried coloring the points using disease severity

--- Clustering/Bacterial/PCA/pca.py
+++ Clustering/Bacterial/PCA/pca.py
@@ -8,26 +8,21 @@ from sklearn import datasets
 
 X=pandas.read_csv("../bacterial_microbiome.csv")
 X.dropna(axis=0,how='any',inplace=True)
-'''
-Y=pandas.read_csv("y_val.csv")
-Y=Y.replace(to_replace="Bronchiectasis",value=0)
-Y=Y.replace(to_replace="Healthy",value=1)
-#
-Y=Y.replace(to_replace="Asian",value=0)
-Y=Y.replace(to_replace="European",value=1)
-Y=Y.replace(to_replace="Control",value=2)
-'''
+
+
+X=X.replace(to_replace="Mild",value=0)
+X=X.replace(to_replace="Moderate",value=1)
+X=X.replace(to_replace="Severe",value=2)
 
 #np.random.seed(5)
 X=X.as_matrix()
 #Y=Y.as_matrix()
 
+y1=X[:,61].astype('int') #BSI Score
+y2=X[:,60].astype('int') #Mild severe and Moderate
+X=X[:,1:60].astype('int') #Data
 
-X=X[:,1:]
 
-#y = Y[:,2].astype('int')
-
-'''
 fig = plt.figure(1, figsize=(4, 3))
 plt.clf()
 ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
@@ -37,22 +32,23 @@ pca = decomposition.SparsePCA(n_components=3)
 pca.fit(X)
 X = pca.transform(X)
 
-#ax.scatter(X[:, 0], X[:, 1],X[:, 2],c=y,s=50)
-ax.scatter(X[:, 0], X[:, 1],X[:, 2],s=50)
+ax.scatter(X[:, 0], X[:, 1],X[:, 2],c=y2,s=50)
 
 ax.w_xaxis.set_ticklabels([])
 ax.w_yaxis.set_ticklabels([])
 ax.w_zaxis.set_ticklabels([])
 
 plt.show()
+
+
 '''
-
-
 #==========2D PCA====================
 pca = decomposition.SparsePCA(n_components=3)
 pca.fit(X)
 X = pca.transform(X)
 
-plt.scatter(X[:, 0], X[:, 1],s=50)
+plt.scatter(X[:, 0], X[:, 1],c=y2,s=50)
 
 plt.show()
+
+'''
