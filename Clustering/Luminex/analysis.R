f2776b3c9f43b99306f4573c8d217a5fa9e504e6
Jayanth-kumar5566 <jayanth.kumar@students.iiserpune.ac.in>
2018-06-18 15:14:24 +0530

    First pass analysis on Luminx data

---
+++ Clustering/Luminex/analysis.R
@@ -0,0 +1,27 @@
+library(tgp)
+library(caret)
+library(corrplot)
+
+data=read.csv("Documents/MS/ML_OMICS/Clustering/Luminex/Luminex.csv")
+data=na.omit(data)
+rownames(data)=data$PatientID
+data=data[,2:43]
+
+trainIndex <- createDataPartition(data$BSI_Bronchiectasis_severity_index, p = 0.8,list = FALSE,times = 1)
+
+
+X_train <- data[trainIndex,c(1:41)]# X
+Z_train <- data[trainIndex,42]
+
+X_test <-data[-trainIndex,c(1:41)]
+Z_test <-data[-trainIndex,42]
+
+# # use a Treed Linear Model (see documentation for more information)
+#fit <- btgpllm(X=X_train, Z=Z_train, XX=X_test)
+fit <- btgpllm(X=X_train, Z=Z_train, XX=X_test)#,basemax=47)
+errors <- sqrt(mean((fit$ZZ.mean - Z_test)^2))
+tgp.trees(fit)
+errors
+
+
+corrplot(cor(data))
