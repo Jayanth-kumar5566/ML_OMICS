b244b3ffe83fff0d27a815a27a81c2b44f15f3d3
Jayanth-kumar5566 <jayanth.kumar@students.iiserpune.ac.in>
2018-07-19 11:50:44 +0800

    New way of preprocessing the Luminex data added. Modified the creation file. Columnwise z-scoring has been added in this version

--- SNF/lumniex_preprocess.py
+++ SNF/lumniex_preprocess.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import pandas
 import numpy
+from scipy.stats import zscore
 df=pandas.read_csv("luminex.csv")
 df.set_index("PatientID",inplace=True)
 def normalize(x):
@@ -8,5 +9,10 @@ def normalize(x):
     for i in range(len(x)):
         x[i]=(x[i]/t)*100
     return x
-df.apply(normalize,axis=1)
+df=df.apply(zscore,axis=0)
+#Translate the values such that all the values are positive
+mini=numpy.min(df.values)
+df=df+abs(mini)
+
+df=df.apply(normalize,axis=1)
 df.to_csv("luminex.csv")
