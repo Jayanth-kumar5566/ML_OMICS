cb18df44050a4c704c2dd72f8a0ed30e16991a3a
Jayanth-kumar5566 <jayanth.kumar@students.iiserpune.ac.in>
2018-07-25 18:23:08 +0800

    Added small corection in code

--- SNF_standard/snf.R
+++ SNF_standard/snf.R
@@ -1,9 +1,9 @@
 library("SNFtool")
 library("vegan")
 library("RColorBrewer")
-b_data=read.csv("Documents/MS/ML_OMICS/SNF/bacteria.csv")
-f_data=read.csv("Documents/MS/ML_OMICS/SNF/fungi.csv")
-l_data=read.csv("Documents/MS/ML_OMICS/SNF/luminex.csv")
+b_data=read.csv("Documents/MS/ML_OMICS/SNF_standard/bacteria.csv")
+f_data=read.csv("Documents/MS/ML_OMICS/SNF_standard/fungi.csv")
+l_data=read.csv("Documents/MS/ML_OMICS/SNF_standard/luminex.csv")
 b_dsim=vegdist(b_data[,-1],method='bray',diag=TRUE,upper=TRUE)
 f_dsim=vegdist(f_data[,-1],method='bray',diag=TRUE,upper=TRUE)
 l_dsim=vegdist(l_data[,-1],method='bray',diag=TRUE,upper=TRUE)
@@ -15,11 +15,11 @@ estimateNumberOfClustersGivenGraph(x)
 displayClusters(x, spectralClustering(x, K = 3))
 W = SNF(list(W1,W2,W3),20,100)
 estimateNumberOfClustersGivenGraph(W)
-displayClusters(W, spectralClustering(W, K = 5))
-C=5
+displayClusters(W, spectralClustering(W, K = 3))
+C=3
 labels=spectralClustering(W,C)
-write.csv(W,"Documents/MS/ML_OMICS/SNF_4/merged_matrix.csv")
-write.csv(labels,"Documents/MS/ML_OMICS/SNF_4/labels.csv")
+write.csv(W,"Documents/MS/ML_OMICS/SNF_standard/merged_matrix.csv")
+write.csv(labels,"Documents/MS/ML_OMICS/SNF_standard/labels.csv")
 
 for (i in 1:229){
   W = SNF(list(W1,W2),i,100)
